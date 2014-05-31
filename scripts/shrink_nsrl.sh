bc110f6667076bb6cae57f917b2f2dc293154f51
blacktop <blacktop@users.noreply.github.com>
2014-05-30 21:18:09 -0600

    Speak UP!!
    I can’t make up my mind

--- scripts/shrink_nsrl.sh
+++ scripts/shrink_nsrl.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # TODO : Also add http://www.mandiant.com/library/RedlineWL//m-whitelist-1.0.zip
-# Grab NSRL Reduced Sets
+echo "[INFO] Downloading NSRL Reduced Sets..."
 wget http://www.nsrl.nist.gov/RDS/rds_2.44/rds_244m.zip 2> /dev/null
-# Unzip NSRL Database zip to /nsrl/
+echo "[INFO] Unzip NSRL Database zip to /nsrl/ ..."
 unzip -uo /rds_244m.zip -d /nsrl/
-rm -f /nsrl/rds_244m.zip
+echo "[INFO] Deleting rds_244m.zip ..."
+rm -f /rds_244m.zip
 # Build bloomfilter from NSRL Database
 cd /nsrl && python /nsrl/build.py
-# Delete large NSRL database files.
+echo "[INFO] Deleting large NSRL database files ..."
 rm -rf /nsrl/minimal/
