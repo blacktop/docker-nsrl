5c1161bfaacc0ec96bdac2cf775c855694bbce4e
blacktop <blacktop@users.noreply.github.com>
2014-11-07 17:58:03 -0700

    Update build.sh to always grab latest nsrl

--- scripts/shrink_nsrl.sh
+++ scripts/shrink_nsrl.sh
@@ -2,11 +2,17 @@
 
 # TODO : Also add http://www.mandiant.com/library/RedlineWL//m-whitelist-1.0.zip
 echo "[INFO] Downloading NSRL Reduced Sets..."
-wget http://www.nsrl.nist.gov/RDS/rds_2.44/rds_244m.zip 2> /dev/null
+NSRL_URL="http://www.nsrl.nist.gov/"
+MIN_SET=$(curl ${NSRL_URL}Downloads.htm 2> /dev/null | \
+  grep -m 1  "Minimal set" | \
+  grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | \
+  sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//')
+wget $NSRL_URL$MIN_SET 2> /dev/null
+# wget http://www.nsrl.nist.gov/RDS/rds_2.44/rds_244m.zip 2> /dev/null
 echo "[INFO] Unzip NSRL Database zip to /nsrl/ ..."
-unzip -uo /rds_244m.zip -d /nsrl/
+unzip -uo /rds_*.zip -d /nsrl/
 echo "[INFO] Deleting rds_244m.zip ..."
-rm -f /rds_244m.zip
+rm -f /rds_*.zip
 # Build bloomfilter from NSRL Database
 cd /nsrl && python /nsrl/build.py
 echo "[INFO] Deleting large NSRL database files ..."
